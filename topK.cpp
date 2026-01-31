dd666617eddfaa359b81a21c87edbb954d6e12c0
struggle <13393443293@163.com>
2026-01-31 16:37:56 +0800

    topK

---
+++ topK.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<queue>
+#include<vector>
+using namespace std;
+int main()
+{
+    vector<int> vec;
+    srand(time(nullptr));
+
+    for(int i=0;i<1000;i++)
+    {
+        vec.push_back(i);
+    }
+    priority_queue<int> maxheap;//大根堆来求最小的topk
+    priority_queue<int,vector<int>,greator<int>>
+    int k=5;
+    for(int i=0;i<5;i++)
+    {
+        maxheap.push(vec[i]);
+    }
+    for(int i=5;i<vec.size();i++)
+    {
+        if(maxheap.top()>vec[i])
+        {
+            maxheap.pop();
+            maxheap.push(vec[i]);
+        }
+    }
+    while(!maxheap.empty())
+    {
+        cout<<maxheap.top()<<endl;
+        maxheap.pop();
+    }
+    return 0;
+}
